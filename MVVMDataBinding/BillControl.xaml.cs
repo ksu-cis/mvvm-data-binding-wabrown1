f2836c6118ad199e567ea825cbad3bdde18ae55d
William Brown <wbrown2492@gmail.com>
2020-04-03 06:16:31 -0500

    MVVM Data Binding

---
+++ MVVMDataBinding/BillControl.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CashRegister;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for BillControl.xaml
+    /// </summary>
+    public partial class BillControl : UserControl
+    {
+        /// <summary>
+        /// The dependency property for the denomination property
+        /// </summary>
+        public static readonly DependencyProperty DenominationProperty =
+            DependencyProperty.Register(
+                "Denomiantion",
+                typeof(Bills),
+                typeof(BillControl),
+                new PropertyMetadata(Bills.One));
+
+        /// <summary>
+        /// The denomination this control displays and modifies
+        /// </summary>
+        public Bills Denomination
+        {
+            get { return (Bills)GetValue(DenominationProperty); }
+            set { SetValue(DenominationProperty, value); }
+        }
+
+        /// <summary>
+        /// The dependency property for quantity
+        /// </summary>
+        public static readonly DependencyProperty QuantityProperty =
+            DependencyProperty.Register(
+                "Quantity",
+                typeof(int),
+                typeof(BillControl),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// Gets or sets the quantity of coin
+        /// </summary>
+        public int Quantity
+        {
+            get { return (int)GetValue(QuantityProperty); }
+            set { SetValue(QuantityProperty, value); }
+        }
+
+        public void OnIncreaseClicked(object sender, RoutedEventArgs e)
+        {
+            Quantity++;
+        }
+
+        public void OnDecreaseClicked(object sender, RoutedEventArgs e)
+        {
+            Quantity--;
+        }
+
+        public BillControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
